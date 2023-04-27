8658be2d7463b7d5b26d0a3dff0a68553f52f8fe
Danila1J <42370968+Danila1J@users.noreply.github.com>
2023-04-27 20:40:17 +0400

    Так как я вернул Application там использовался старый конструктор для Kotik без параметра. Исправил и удалил вызов setName для Kotik

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,8 +3,8 @@ import animals.Kotik;
 public class Application {
     public static void main(String[] args) {
         Kotik kotik1 = new Kotik("Мурзик", "Мяу", 10, 5500);
-        Kotik kotik2 = new Kotik();
-        kotik2.setName("Барсик");
+        Kotik kotik2 = new Kotik("Барсик");
+        //kotik2.setName("Барсик");
         kotik2.setVoice("Мяу");
         kotik2.setSatiety(5);
         kotik2.setWeight(4500);
